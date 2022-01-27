9f33e9e218b548dfb85d744dac4292b32048aea1
muhenge <mugunga5@gmail.com>
2022-01-27 16:43:46 +0200

    header

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import First from './First/First'
 import './vendor/@fortawesome/fontawesome-free/css/all.min.css';
 import './dashboard/assets/vendor/nucleo/css/nucleo.css'
 import './vendor/prismjs/themes/prism.css';
-// import './front/css/front.css';
+import './front/css/front.css'
 function App() {
   return (
     <div>
--- src/First/First.js
+++ src/First/First.js
@@ -1,13 +1,56 @@
-import React from 'react';
-
+import React from "react";
+import "./First.css";
 const First = () => {
   return (
     <>
-    <header className="vh-100">
-    
-    </header>
+      <header id="header-one" className="vh-100 position-relative">
+        <nav className="navbar navbar-light bg-light">
+          <a className="navbar-brand" href="#">
+            Navbar
+          </a>
+          <div className="d-flex justify-content-end ">
+            <div className="">
+              
+            </div>
+
+            <div className="">
+              <button className="btn btn-primary">Get Started</button>
+            </div>
+          </div>
+        </nav>
+        <div class="d-flex position-absolute top-50 start-50 translate-middle">
+          <span className="text-light display-3-md text-center">
+            We help Businesses from any Country Rank Locally in other Countries Worldwide by providing an International SEO solution.
+          </span>
+        </div>
+        <div className="position-absolute top-100 start-50 translate-middle w-50">
+        <div className="row">
+            <div className="col">
+              <div className="card shadow p-5">
+                <div className="card-body">
+                </div>
+              </div>
+            </div>
+            <div className="col ">
+              <div className="card shadow p-5">
+                <div className="card-body">
+                </div>
+              </div>
+            </div>
+            <div className="col ">
+              <div className="card shadow p-5">
+                <div className="card-body">
+                </div>
+              </div>
+            </div>
+          </div>
+          </div>
+      </header>
+      <section className="">
+        
+      </section>
     </>
   );
-}
+};
 
 export default First;
