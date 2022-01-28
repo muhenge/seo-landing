5ada660b9e86b901b538d5586f95c929e56b6ad8
muhenge <mugunga5@gmail.com>
2022-01-28 17:32:50 +0200

    Second

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import First from './First/First'
+import First from './First/First';
+import Second from './Second/Second';
+
 import './vendor/@fortawesome/fontawesome-free/css/all.min.css';
 import './dashboard/assets/vendor/nucleo/css/nucleo.css'
 import './vendor/prismjs/themes/prism.css';
@@ -6,7 +8,8 @@ import './front/css/front.css'
 function App() {
   return (
     <div>
-      <First />
+      {/* <First /> */}
+      <Second />
     </div>
   );
 }
--- src/Second/Second.js
+++ src/Second/Second.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import  Header from "../Header/Header.js";
-
+import './second.css'
 const Second = () => {
   return (
     <div>
       <Header id="header-two">
-
+      <div className="position-absolute top-50 start-50 translate-middle">
+          <span className="text-light display-3-md text-center">
+            We help Businesses from any Country rank locally in other countries worldwide by providing an international SEO solution.
+          </span>
+          <div className="">
+            <button className="d-flex justify-content-center align-items-center btn btn-warning text-black round mt-5">Start for free</button>
+          </div>
+        </div>
       </Header>
     </div>
   );
